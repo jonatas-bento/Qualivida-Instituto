9a93be6711b1c6888d221f9f0b7571d2a0a4a5bd
Jonatas Bento <pr.jmbento@gmail.com>
2025-03-24 09:26:55 -0300

    update data for about

--- src/app/components/sobre/sobre.component.ts
+++ src/app/components/sobre/sobre.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-sobre',
+  styleUrls: ['./sobre.component.css'],
   templateUrl: './sobre.component.html',
-  styleUrls: ['./sobre.component.css']
+  encapsulation: ViewEncapsulation.None
 })
 export class SobreComponent implements OnInit {
 
